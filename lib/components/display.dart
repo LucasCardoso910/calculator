6ae11b67bffe5749ab7b3607526ef7efcfef1e7e
Lucas Cardoso <lucas.cardoso.santos@usp.br>
2022-05-14 00:26:56 -0300

    improving display component
    Signed-off-by: Lucas Cardoso <lucas.cardoso.santos@usp.br>

--- lib/components/display.dart
+++ lib/components/display.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class Display extends StatelessWidget {
@@ -7,8 +8,36 @@ class Display extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(text),
+    const Color bgColor = Color.fromRGBO(48, 48, 48, 1);
+    const displayTextStyle = TextStyle(
+      fontWeight: FontWeight.w100,
+      decoration: TextDecoration.none,
+      fontSize: 80,
+      color: Colors.white,
+    );
+
+    return Expanded(
+      flex: 1, // takes all the space available
+      child: Container(
+        color: bgColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AutoSizeText(
+                text,
+                minFontSize: 20,
+                maxFontSize: 80,
+                maxLines: 1,
+                textAlign: TextAlign.end,
+                style: displayTextStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
