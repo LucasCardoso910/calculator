78789bd3052b10c6e42d11aeb79f4bbd8abdb444
Lucas Cardoso <lucas.cardoso.santos@usp.br>
2022-05-14 00:35:09 -0300

    create void buttons just to show them for now
    Signed-off-by: Lucas Cardoso <lucas.cardoso.santos@usp.br>

--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -5,10 +5,20 @@ class Keyboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const buttonLabel = Text('9');
     Size mediaSize = MediaQuery.of(context).size; // get screen size
 
     return Container(
       height: mediaSize.height * 0.6, // set height as 60% of screen height
+      child: Column(
+        children: [
+          ElevatedButton(onPressed: () {}, child: buttonLabel),
+          ElevatedButton(onPressed: () {}, child: buttonLabel),
+          ElevatedButton(onPressed: () {}, child: buttonLabel),
+          ElevatedButton(onPressed: () {}, child: buttonLabel),
+          ElevatedButton(onPressed: () {}, child: buttonLabel),
+        ],
+      ),
     );
   }
 }
