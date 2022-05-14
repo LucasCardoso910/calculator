fbffa6fa6815d72338b406009f09503faf2547e1
Lucas Cardoso <lucas.cardoso.santos@usp.br>
2022-05-14 02:04:25 -0300

    create clear button logic
    Signed-off-by: Lucas Cardoso <lucas.cardoso.santos@usp.br>

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -2,7 +2,16 @@ class Memory {
   String _value = '0';
 
   void applyCommand(String command) {
+    if (command == 'AC') {
+      _allClear();
+      return;
+    }
+
     _value += command;
+  }
+
+  _allClear() {
+    _value = '0';
   }
 
   String get value => _value;
