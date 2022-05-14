9dd3f727ba7d14989b87e97bc8e0f3eb26a82031
Lucas Cardoso <lucas.cardoso.santos@usp.br>
2022-05-14 01:46:41 -0300

    convert calculator to stateful widget
    Signed-off-by: Lucas Cardoso <lucas.cardoso.santos@usp.br>

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,10 +1,18 @@
 import 'package:calculator/components/display.dart';
 import 'package:calculator/components/keyboard.dart';
+import 'package:calculator/models/memory.dart';
 import 'package:flutter/material.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
   const Calculator({Key? key}) : super(key: key);
 
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+  
   void _onPressed(String text) {
     print(text);
   }
