c50946df39f6f10510d57cccb7d3a21767846780
Lucas Cardoso <lucas.cardoso.santos@usp.br>
2022-05-22 23:47:53 -0300

    add digit function
    Signed-off-by: Lucas Cardoso <lucas.cardoso.santos@usp.br>

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,17 +1,43 @@
 class Memory {
+  static const operations = const ['%', '/', 'x', '-', '+', '='];
+
+  final _buffer = [0.0, 0.0];
+  int bufferIndex = 0;
+  String operation = '';
   String _value = '0';
+  bool _wipeValue = true;
 
   void applyCommand(String command) {
     if (command == 'AC') {
       _allClear();
       return;
+    } else if (operations.contains(command)) {
+      _setOperation(command);
+    } else {
+      _addDigit(command);
     }
+  }
 
-    _value += command;
+  _setOperation(String newOperation) {
+    _wipeValue = true;
+  }
+
+  _addDigit(String digit) {
+    final isDot = digit == ',';
+    if (isDot && _value.contains(',') && !_wipeValue) return;
+
+    final emptyValue = isDot ? '0' : '';
+    if (_wipeValue) _value = emptyValue;
+
+    _value += digit;
+    _wipeValue = false;
+
+    _buffer[bufferIndex] = double.tryParse(_value) ?? 0;
   }
 
   _allClear() {
     _value = '0';
+    _wipeValue = true;
   }
 
   String get value => _value;
